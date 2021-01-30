266182e98d84da1b234edfcafaf8771103a57c44
ivanov-in <work.ivanov.in@gmail.com>
2021-01-31 01:50:16 +0300

    add client realization

--- server/src/ru/ivanovin/chat/server/ChatServer.java
+++ server/src/ru/ivanovin/chat/server/ChatServer.java
@@ -1,7 +1,52 @@
 package ru.ivanovin.chat.server;
 
-public class ChatServer {
+import ru.ivanovin.network.TCPConnection;
+import ru.ivanovin.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class ChatServer implements TCPConnectionListener {
+
     public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e) {
+                    System.out.println("TCPConnection exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
 
     }
 }
